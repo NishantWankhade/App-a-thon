91f5b23c84cb477a303758b66e834cae989e8481
NGW334 <ngw334@gmail.com>
2023-10-07 13:08:33 +0530

    Observing keypoints for a static image

--- camera_feed.py
+++ camera_feed.py
@@ -1,18 +1,20 @@
 import cv2
 import pose_detection
 
-# Create a VideoCapture object to access the default camera (usually camera index 0)
-cap = cv2.VideoCapture(0)
+# # Create a VideoCapture object to access the default camera (usually camera index 0)
+# cap = cv2.VideoCapture(0)
+img = cv2.imread("1.jpg")
+frame = pose_detection.process(img)
 
 while True:
-    # Read a frame from the camera
-    ret, frame = cap.read()
+    # # Read a frame from the camera
+    # ret, frame = cap.read()
 
-    if not ret:
-        break
+    # if not ret:
+    #     break
 
-    # Process for pose detection on each frame
-    frame = pose_detection.process(frame)
+    # # Process for pose detection on each frame
+    # frame = pose_detection.process(frame)
 
     # Display the captured frame
     cv2.imshow("Camera Feed", frame)
@@ -21,6 +23,6 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
-# Release the VideoCapture and close the OpenCV window
-cap.release()
-cv2.destroyAllWindows()
+# # Release the VideoCapture and close the OpenCV window
+# cap.release()
+# cv2.destroyAllWindows()
