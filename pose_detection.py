a7e92c5f788b29e549a5457db41783322dae0405
NGW334 <ngw334@gmail.com>
2023-10-07 12:26:54 +0530

    Pose detection using pretrained model

---
+++ pose_detection.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+
+def process(img):
+    # Load a model
+    model = YOLO('yolov8n-pose.pt')  # load an official model
+    # model = YOLO('path/to/best.pt')  # load a custom model
+
+    # Predict with the model
+    results = model(img)  # predict on an image
+
+    annotated_frame = results[0].plot()
+
+    return annotated_frame
