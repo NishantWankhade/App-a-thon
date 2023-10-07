239d17abecd64876b4319d40b73f5293342f6dc1
NGW334 <ngw334@gmail.com>
2023-10-07 13:01:39 +0530

    Observing Keypoints detected

--- pose_detection.py
+++ pose_detection.py
@@ -9,5 +9,7 @@ def process(img):
     results = model(img)  # predict on an image
 
     annotated_frame = results[0].plot()
+    for r in results:
+        print(r.keypoints)
 
     return annotated_frame
