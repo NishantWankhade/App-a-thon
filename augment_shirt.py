af8186caea36a5af0b42d7bcd71d498587ffa17a
NGW334 <ngw334@gmail.com>
2023-10-09 15:55:05 +0530

    Progress regarding augmentation with respect to the shoulder and waist keypoints

--- augment_shirt.py
+++ augment_shirt.py
@@ -3,21 +3,26 @@ import pose_model_class
 import resize_image
 
 def augment(person_image , pose):
-    t_shirt = cv2.imread("T-shirt.jpg", cv2.IMREAD_UNCHANGED)
+    t_shirt = cv2.imread("T-shirt.jpg")
 
     # Scale image according to pose
-    # person_image = resize_image.scaleImg(person_image, pose)
+    t_shirt = resize_image.scaleImg(t_shirt, pose)
+    print(t_shirt.shape)
 
-    pose.show_points()
-    # result_image = person_image.copy()
-    # alpha_mask = t_shirt[:, :, 3] / 255.0  # Extract the alpha channel
-    
-    # for c in range(0, 3):  # Apply blending for RGB channels
-    #     result_image[:, :, c] = (1.0 - alpha_mask) * result_image[:, :, c] + alpha_mask * t_shirt[:, :, c]
+    result_image = person_image.copy()
+    shape = t_shirt.shape
 
-    # # Display or save the augmented image
-    # return result_image
+    h = shape[0]
+    w = shape[1]
+    input_shape = person_image.shape
+    input_height = input_shape[0]
+    input_width = input_shape[1]
 
-    return person_image
+    if(h <= input_height and w <= input_width):
+        result_image[0:h, 0:w] = t_shirt
+        return result_image
+    else :
+        return person_image
+
 
     
--- resize_image.py
+++ resize_image.py
@@ -1,8 +1,18 @@
 import cv2
-import pose_model_class
 
 def scaleImg(img, pose):
     shoulder = pose.shoulder
     waist = pose.waist
+    
+    width = abs(shoulder[1][0] - shoulder[0][0])
+    height = abs(shoulder[1][1] - waist[1][1])
+    
+    print(height)
+    print(width)
+
+    if(height >= width) :
+        img = cv2.resize(img, (width,height))
+
+    return img
 
     
